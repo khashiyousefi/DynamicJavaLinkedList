24ff79b29a409c0e8ad86f9590eb1e75178d2082
kashy <kashy@sfu.ca>
2020-06-18 22:48:16 -0700

    Node getter and setter

--- src/com/SixLink/LinkedList.java
+++ src/com/SixLink/LinkedList.java
@@ -1,6 +1,32 @@
 package com.SixLink;
 
+
 public class LinkedList {
     private Node first;
     private Node last;
+//    public LinkedList(){
+//
+//    }
+
+    public void addLast(int item){
+        Node node = new Node();
+        node.setValue(item);
+
+    }
+    public void addFirst(){
+
+    }
+    public void deleteFirst(){
+
+    }
+    public void deleteLast(){
+
+    }
+    public void contains(){
+
+    }
+    public void indexOf(){
+
+    }
+
 }
--- src/com/SixLink/Node.java
+++ src/com/SixLink/Node.java
@@ -2,5 +2,19 @@ package com.SixLink;
 
 public class Node {
     private int value;
-    private int next;
+    private Node next;
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+    public int getValue(){
+        return value;
+    }
+    public void setNode(Node next) {
+        this.next = next;
+    }
+
+    public Node getNext(){
+        return next;
+    }
 }
